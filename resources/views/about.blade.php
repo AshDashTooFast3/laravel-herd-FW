44ecc3e6bfb283b0d0a8efd9d8c9753cc7cc632f
AshDashTooFast3 <338127@student.mboutrecht.nl>
2025-09-16 13:42:11 +0200

    Les 6 af: routing van views voor joblisting gemaakt + via variabels tekst geschreven

--- resources/views/about.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot name="heading">About</x-slot>
-    <h1>Hello from the About page</h1>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,55 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Senior Developer',
+                'salary' => '120.000',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Teacher',
+                'salary' => '80.000',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Project Manager',
+                'salary' => '100.000',
+            ],
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Senior Developer',
+            'salary' => '120.000',
+        ],
+        [
+            'id' => 2,
+            'title' => 'Teacher',
+            'salary' => '80.000',
+        ],
+        [
+            'id' => 3,
+            'title' => 'Project Manager',
+            'salary' => '100.000',
+        ]
+    ];
+
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
