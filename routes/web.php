b5ee069e5d428e6b98e8787290fa603b953b1f09
AshDashTooFast3 <338127@student.mboutrecht.nl>
2025-09-29 10:52:56 +0200

    Les 13 af: Eager loading toegepast aan onze app + debug-navbar (zit in vorige commit)

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-
-
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function ()  {
-    return view('jobs', 
-     ['jobs' => Job::all()]);
+Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->get();
+    return view('jobs', [
+        'jobs' => $jobs,
+    ]);
 });
 
-
-Route::get('/jobs/{id}', function ($id)  {
-
-    $job = Job::find($id);    
-
-    return view('job', ['job' => $job]);
-
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+    return view('job', [
+        'job' => $job,
+    ]);
 });
 
 Route::get('/contact', function () {
